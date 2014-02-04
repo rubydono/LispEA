9119e3b20d6c472b0809255a547fefab1f762d8f
RubyTuesdayDONO <ReubenGarrett@Gmail.com>
2014-02-04 00:34:28 -0600

    Explore Bash instrumentation capabilities
    explored instrumentation capabilities that Bash exposes
    via its `extdebug` shopt [1] and `trap` builtin [2],
    which i discovered while evaluating bashdb [3]
    as suggested by Eric Blake on the Bash users' mailing list [4].
    
    so far, it's been almost trivially easy to accomplish many of the
    instrumentation goals i'm seeking, such as intercepting stdout and
    rewriting commands (much like AOP's before/after/around advice).
    
    [1]: http://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
    [2]: http://www.gnu.org/software/bash/manual/html_node/Bourne-Shell-Builtins.html#index-trap
    [3]: http://bashdb.sourceforge.net/
    [4]: https://lists.gnu.org/archive/html/help-bash/2014-01/msg00012.html

--- util/repl/cirebi/cirebi.sh
+++ util/repl/cirebi/cirebi.sh
@@ -1,2 +1,18 @@
-#!/bin/bash -e
-exit 1
+#!/bin/bash --debugger
+
+debugTrap(){
+    echo +=== $BASH_COMMAND: >&2
+    cirebi_trapped_cmd="$BASH_COMMAND"
+    stdout="$( eval $cirebi_trapped_cmd )" 
+    retval=$? 
+    cat <<<"$stdout" 
+    echo ===+ $retval >&2
+    return 1
+}
+
+trap debugTrap DEBUG
+
+echo test
+ls -latch
+/sbin/ip -f inet addr show
+false
